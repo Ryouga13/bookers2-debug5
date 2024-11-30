28b2478813862c5b189595d459d5fd8302e1a4b7
EC2 Default User <ec2-user@ip-172-31-37-105.eu-north-1.compute.internal>
2024-11-30 04:23:12 +0000

    bookers2-debug5 Finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
+  
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+  
+
 
   #<--------　フォロー定義　-------->
 
@@ -42,12 +47,27 @@ class User < ApplicationRecord
 
   #<--------　フォロー定義　終了-------->
 
+  #<--------　検索機能定義　-------->
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
+  #<--------　検索機能定義　終了-------->
 
 
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50 }
-  
+
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
